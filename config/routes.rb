73ef7513b8714bd9149d6d5a094f1f72a31f921f
seiya1911 <n.siy1911cb@gmail.com>
2021-06-16 10:10:10 +0000

    [Fix]adminログインページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins, class_name: "Admin::Admin"
-  devise_for :customers, class_name: "Public::Customer"
+  devise_for :admins, :controllers => {
+    :sessions => 'admins/sessions'
+  }
+
+  devise_for :customers, class_name: "Public::Customer", :controllers => {
+    :sessions => 'customers/sessions',
+    :registrations => 'customers/registrations',
+   }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'products#top'
