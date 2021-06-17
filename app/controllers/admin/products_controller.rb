bdb40ce414fec285b00cf53c1d0503b2c50e7838
seiya1911 <n.siy1911cb@gmail.com>
2021-06-17 08:40:03 +0000

    [Add]admin/products/index,new,show,edit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,21 +1,37 @@
 class Admin::ProductsController < ApplicationController
-  
+
   def index
+    @products = Product.all
   end
-  
+
   def show
+    @product = Product.find(params[:id])
   end
-  
+
   def new
+    @produt = Product.new
+    @genres = Genre.all
   end
-    
+
   def create
+    @product = Product.new(product_params)
+    @product.save!
   end
-  
+
   def edit
   end
-  
+
   def update
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: 'You have updated genre successfully.'
+    else
+      render 'edit'
+    end
   end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :image, :description, :price, :genre_id, :status)
+    end
 
 end
