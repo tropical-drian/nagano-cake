634e729cb769b3a6c11f8254d3fbb68671bce32c
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-06-17 04:51:33 +0000

    [fix]ユーザー新規登録の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # 新規登録の時の保存機能
+    def configure_permitted_parameters
+      added_attrs = [:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :postal_code, :residence, :phone_number, :password]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    end
+
 end
