cf0eb3195421db9df597b5944e35d1b3aa65a9bb
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-17 05:42:10 +0000

    public update

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,18 @@
 class Public::ProductsController < ApplicationController
+
+  before_action :authenticate_customer!,only: [:show]
+
+  def top
+    @products = Product.all.order(created_at: :asc) #:asc,古い順
+  end
+
+  def about
+  end
+
+  def index
+  end
+
+  def show
+  end
+
 end
