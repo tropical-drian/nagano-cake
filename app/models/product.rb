fc4ff0847f085e3b4d2a7869b1b6b5a961512aeb
seiya1911 <n.siy1911cb@gmail.com>
2021-06-19 02:37:52 +0000

    [Add]admin/products/new.action

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
-  
-   belongs_to :genre
-   
+  belongs_to :genre
+
   attachment :image
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a762d410bbe802da17caf946752d6ae366dbf110ad370f706a794e6748ab438a475a5d83825d7b6a40550720acbdec5da206e16852c75733eef5c807946ea973'
