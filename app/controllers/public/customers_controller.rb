3f80b224c2a23b42925039876a322a597751f2c0
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-06-19 02:24:42 +0000

    [add]ユーザーマイページ、登録情報編集画面、退会画面作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,33 @@
 class Public::CustomersController < ApplicationController
 
   def show
+    @customer = current_customer
   end
 
   def withdraw
   end
 
   def out
+    @customer = current_customer
+    @customer.update(is_valid: true)
+    reset_session
+    redirect_to root_path
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_path(current_customer)
   end
+
+  private
+
+	def customer_params
+	  params.permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :email, :postal_code, :residence, :phone_numbar)
+	end
 
 end
--- app/models/public/customer.rb
+++ app/models/public/customer.rb
@@ -3,5 +3,10 @@ class Public::Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  # 退会機能
+  def active_for_authentication?
+    super && (self.is_valid == false)
+  end
+
 end
