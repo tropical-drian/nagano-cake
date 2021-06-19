f0596ddae3ea4f587395e48293d924fe7613d003
seiya1911 <n.siy1911cb@gmail.com>
2021-06-19 05:46:43 +0000

    [Fix]admin/genre/edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -20,6 +20,7 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
+    @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
       redirect_to admin_genres_path, notice: 'You have updated genre successfully.'
     else
