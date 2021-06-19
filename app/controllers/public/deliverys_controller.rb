d2f93f026cca24bd48d20a4ccd811e088e9b17af
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-19 03:53:20 +0000

    [make delivery form/controller ]

--- app/controllers/public/deliverys_controller.rb
+++ app/controllers/public/deliverys_controller.rb
@@ -1,2 +1,28 @@
 class Public::DeliverysController < ApplicationController
+
+  def create
+    @delivery = Delivery.new(delivery_params)
+    @delivery.customer_id = current_customer.id
+    if  @delivery.save
+      flash[:notice] = "配達先を登録しました"
+      redirect_to  delivery_path
+    else
+      flash[:notice] = "必要情報を入力してください／ハイフンは使用できません"
+      redirect_to  delivery_path
+    end
+  end
+
+  def index
+    @delivery = Delivery.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
