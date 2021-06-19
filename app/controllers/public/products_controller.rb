7ad7f5dc1d63d35586e064e6637a7e2af504810f
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-19 06:50:17 +0000

    [Update products_index]

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -4,17 +4,20 @@ class Public::ProductsController < ApplicationController
 
   def top
     @products = Product.all.order(created_at: :asc) #:asc,古い順
+    @genres   = Genre.
   end
 
   def about
   end
 
   def index
+    @genres   = Genre.all
     @products = Product.where(is_sale: true).page(params[:page]).per(8)
   end
 
   def show
-    @product = Product.find(params[:id])
+    @products = Product.all
+    @product  = Product.find(params[:id])
   end
 
 end
