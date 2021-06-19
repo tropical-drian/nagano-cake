d9225dd259e2189daff929606ba4d40fe3338a67
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-19 08:53:45 +0000

    [Update public/products index showページ作成]

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -4,20 +4,27 @@ class Public::ProductsController < ApplicationController
 
   def top
     @products = Product.all.order(created_at: :asc) #:asc,古い順
-    @genres   = Genre.
+    @genres = Genre.all
   end
 
   def about
   end
 
   def index
-    @genres   = Genre.all
-    @products = Product.where(is_sale: true).page(params[:page]).per(8)
+    @genres = Genre.all
+    @products = Product.where(status: true).page(params[:page]).per(8)
   end
 
   def show
     @products = Product.all
-    @product  = Product.find(params[:id])
+    @product =  Product.find(params[:id])
+    @cart_item = CartItem.new
   end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :image, :description, :price, :genre_id, :status, :genre_id)
+    end
 
 end
