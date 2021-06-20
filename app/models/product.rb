aad574e9daa8080d65f5bc8dcd2c54f6999f470d
seiya1911 <n.siy1911cb@gmail.com>
2021-06-20 02:25:51 +0000

    [Add]searchフォーム

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ApplicationRecord
 	has_many :orders, through: :ordered_products
 	has_many :ordered_products
   attachment :image
+
+  # 検索用メソッド
+  def self.search(keyword)
+  where(["name like? OR description like?", "%#{keyword}%", "%#{keyword}%"])
+  end
 end
