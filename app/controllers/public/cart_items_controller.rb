47e3995365341855db7479a89dac5aee7c8b30ca
seiya1911 <n.siy1911cb@gmail.com>
2021-06-21 10:03:51 +0000

    [Update]cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -16,21 +16,22 @@ class Public::CartItemsController < ApplicationController
     @total = product_total_price(@cart_items).to_s(:delimited)
   end
 
-  def create
-    @cart_item = current_customer.cart_items.new
-    # カートの中に同じ商品がある場合は数だけ追加する
-    @add_cart_item =  CartItem.find_by(product: @cart_item.product)
-    if @add_cart_item.present? && @cart_item.valid?
-      @cart_item.quantity += @add_cart_item.quantity
-      @add_cart_item.destroy
-    end
-  end
-  
+  # def create
+  #   @cart_item = current_customer.cart_items.new
+  #   # カートの中に同じ商品がある場合は数だけ追加する
+  #   @add_cart_item =  CartItem.find_by(product: @cart_item.product)
+
+  #   end
+  # end
+
   def create
     @cart_item = current_customer.cart_items.new(params_cart_item)
     @update_cart_item = CartItem.find_by(product: @cart_item.product)
+    if @update_cart_item.present? && @cart_item.valid?
+       @cart_item.quantity += @update_cart_item.quantity
+       @add_cart_item.destroy
+    end
 
-    @cart_item.save
     flash[:notice] = "#{@cart_item.product.name}をカートに追加しました"
     redirect_to cart_items_path
 	end
@@ -40,10 +41,15 @@ class Public::CartItemsController < ApplicationController
     redirect_to current_cart_items
   end
 
-  def destroy_all
-    @cart_items = current_customer.cart_items
-    @cart_items.destroy_all
-    redirect_to current_cart_items
+
+  def all_destroy
+    @cart = current_cart
+    @cart.destroy
+    session[:cart_id] = nil
+    respond_to do |format|
+      format.html { redirect_to cart_items_path, notice: 'カートが空になりました。' }
+      format.json { head :no_content }
+    end
   end
 
   private
