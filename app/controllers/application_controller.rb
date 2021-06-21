eb4c81c20d0bc004e7b8272b3065d164fa1b001b
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-06-21 05:29:08 +0000

    [add]カート表示機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,35 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # 税込の計算
+  # floorは小数点切り捨て
+  def tax_price(price)
+    (price * 1.1).floor
+  end
+
+  # 小計の計算
+  def subtotal(sub)
+    (tax_price(sub.product.price) * sub.quantity)
+  end
+
+   # 商品合計金額の計算
+  def product_total_price(products)
+    sum = 0
+    products.each do |product|
+     sum += subtotal(product)
+    end
+    return price
+  end
+
+  # 請求額の計算
+  def billing(order)
+    product_total_price(current_cart) + order.postage
+  end
+
+  def current_cart
+    @cart_items = current_customer.cart_items
+  end
+
   protected
 
   # 新規登録の時の保存機能
@@ -15,16 +44,52 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
     end
 
-  def
-    after_sign_out_path_for(resource_or_scope)
-      if resource_or_scope == :customer
-          root_path
-      elsif resource_or_scope == :admin
-        new_admin_session_path
-      else
-        root_path
-      end
+class ApplicationController < ActionController::Base
+<<<<<<< HEAD
+=======
+
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 税込の計算
+  # floorは小数点切り捨て
+  def tax_price(price)
+    (price * 1.1).floor
   end
 
->>>>>>> [Fix]admnin権限
+  # 小計の計算
+  def subtotal(sub)
+    (tax_price(sub.product.price) * sub.quantity)
+  end
+
+   # 商品合計金額の計算
+  def product_total_price(products)
+    sum = 0
+    products.each do |product|
+     sum += subtotal(product)
+    end
+    return price
+  end
+
+  # 請求額の計算
+  def billing(order)
+    product_total_price(current_cart) + order.postage
+  end
+
+  def current_cart
+    @cart_items = current_customer.cart_items
+  end
+
+  protected
+
+  # 新規登録の時の保存機能
+  # 会員情報編集時の保存機能
+    def configure_permitted_parameters
+      added_attrs = [:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :postal_code, :residence, :phone_number, :password]
+      devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+      devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+    end
+
+
+
 end
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,18 @@
 class Public::CartItemsController < ApplicationController
+<<<<<<< HEAD
   
   
+=======
+
+  before_action :set_cart_item, only: [:update, :destroy]
+  before_action :authenticate_customer!
+
+>>>>>>> [add]カート表示機能
   def index
     @cart_items = current_customer.cart_items
   end
 
+<<<<<<< HEAD
 	def update
 	end
 
@@ -22,6 +30,35 @@ class Public::CartItemsController < ApplicationController
 
 	def destroy
 	end
+=======
+  def update
+    @cart_item.update(quantity: params[:cart_item][:quantity].to_i)
+    @price = subtotal(@cart_item).to_s(:delimited)
+    @cart_items = current_cart
+    @total = product_total_price(@cart_items).to_s(:delimited)
+  end
+
+  def create
+    @cart_item = current_customer.cart_items.new
+    # カートの中に同じ商品がある場合は数だけ追加する
+    @add_cart_item =  CartItem.find_by(product: @cart_item.product)
+    if @add_cart_item.present? && @cart_item.valid?
+      @cart_item.quantity += @add_cart_item.quantity
+      @add_cart_item.destroy
+    end
+  end
+
+  def destroy
+    @cart_item.destroy
+    redirect_to current_cart_items
+  end
+
+  def destroy_all
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+    redirect_to current_cart_items
+  end
+>>>>>>> [add]カート表示機能
 
   private
 
