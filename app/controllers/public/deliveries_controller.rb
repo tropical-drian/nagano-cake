874135f4c7353503269c50f8ce77cdc2d1ffa94b
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-21 06:57:33 +0000

    [fix delivery error]

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,26 +1,22 @@
 class Public::DeliveriesController < ApplicationController
 
   def create
-
-    @delivery = Delivery.new(delivery_params)
-    #@delivery = current_cust.new
-    #@delivery = Delivery.new( params.require(:delivery).permit(:name, :postcode, :address, :customer_id))
-        #@payment = current_user.payments.new
-    @delivery.customer_id = current_customer.id
+      @delivery = Delivery.new(delivery_params)
+      @delivery.customer_id = current_customer.id
     if @delivery.save
-    flash[:notice] = "配達先を登録しました"
-     redirect_to deliveries_path
+      flash[:notice] = "配達先を登録しました"
+      redirect_to deliveries_path
     else
-    #@delivery = Delivery.new
-    #@delivery.customer_id = current_customer.id
-     flash[:notice] = "必要情報を入力してください／ハイフンは使用できません"
-     redirect_to  deliveries_path
+      @delivery = Delivery.new
+      @delivery.customer_id = current_customer.id
+      flash[:notice] = "必要情報を入力してください／ハイフンは使用できません"
+      redirect_to deliveries_path
     end
   end
 
   def index
-    @delivery = Delivery.new
-    @a = Delivery.where(customer_id: current_customer.id)
+      @delivery = Delivery.new
+      @delivery_info = Delivery.where(customer_id: current_customer.id)
   end
 
   def edit
@@ -31,9 +27,11 @@ class Public::DeliveriesController < ApplicationController
      @delivery = Delivery.find(params[:id])
       if @delivery.update(delivery_params)
         flash[:notice] = "配達先を編集しました"
-        redirect_to deliverys_path
+        redirect_to deliveries_path
       else
-        render action: :edit
+        @delivery = Delivery.find(params[:id])
+        flash[:notice] = "必要情報を入力してください／ハイフンは使用できません"
+        redirect_to deliveries_path
       end
   end
 
@@ -41,7 +39,7 @@ class Public::DeliveriesController < ApplicationController
      @delivery = Delivery.find(params[:id])
      @delivery.destroy
      flash[:notice] = "配達先を編集しました"
-     redirect_to deliverys_path
+     redirect_to deliveries_path
   end
 
   private
