f88575c5cc4b3b5985b48d8870d7d65c6a6afde6
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-06-22 10:06:44 +0000

    [fix]お届け先選択の修正

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,7 +2,7 @@ class Delivery < ApplicationRecord
 
   # orders/newで使用
 	def delivery_address
-		self.postal_code + self.address + self.name
+		self.postcode + self.address + self.name
 	end
   belongs_to :customer
 
