621f84b2c4ec69c5f138c6f074c7b8af3dbacc6b
yunhao17 <yunhao17@gmail.com>
2021-06-23 05:26:51 +0000

    [update]コード更新

--- app/controllers/admin/ordered_products_controller.rb
+++ app/controllers/admin/ordered_products_controller.rb
@@ -2,8 +2,9 @@ class Admin::OrderedProductsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-    @ordered_product.update(production_status: params[:production_status])
-    redirect_to admin_order_path(@order)
+    @ordered_product = OrderedProduct.find(params[:id])
+    @ordered_product.update(ordered_product_params)
+    redirect_to admin_order_path(@ordered_product.order)
   end
 
   private
