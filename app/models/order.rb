2cb7040337f0bbc93806087b02551bb5ce39524e
seiya1911 <n.siy1911cb@gmail.com>
2021-06-24 02:51:28 +0000

    [Update]注文・制作ステータス連携

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,13 @@ class Order < ApplicationRecord
   enum payment_method: {"クレジットカード": 0,"銀行振込": 1}
 	enum status: {"入金待ち": 0,"入金確認": 1,"製作中": 2,"発送準備中": 3, "発送済み": 4}
 
+	 #製作ステータスの変更
+  def change_production_status
+    if self.status == "入金待ち"
+      self.ordered_products.update_all(production_status: :着手不可)
+    elsif self.status == "入金確認"
+      self.ordered_products.update_all(production_status: :製作待ち)
+    end
+  end
+
 end
--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -5,4 +5,14 @@ class OrderedProduct < ApplicationRecord
 
 	enum production_status: {"着手不可": 0,"製作待ち": 1,"製作中": 2,"製作完了": 3}
 
+	#注文ステータスの変更
+  def change_status
+    products = self.order.ordered_products
+    if self.production_status == "製作中"
+      self.order.update(status: "製作中")
+    elsif products.pluck(:production_status).all?{ |status| status == "製作完了"}
+      self.order.update(status: "発送準備中")
+    end
+  end
+
 end
