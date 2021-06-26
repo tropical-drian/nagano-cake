e56b752f3dd81d820f469d1af0de739401aca0f6
seiya1911 <n.siy1911cb@gmail.com>
2021-06-26 03:47:11 +0000

    [Fix]tests.error

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -3,7 +3,8 @@ class Public::ProductsController < ApplicationController
   before_action :authenticate_customer!,only: [:show]
 
   def top
-    @products = Product.all.order(created_at: :asc) #:asc,古い順
+    @products = Product.all.order(created_at: :asc)#:asc,古い順
+    @genres = Genre.all
   end
 
   def about
